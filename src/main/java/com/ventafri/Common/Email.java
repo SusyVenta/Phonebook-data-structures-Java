9bc084bf21ade67088d35e4d0cc67efbda6df1c5
susy <susanna.ventafridda@gmail.com>
2019-05-19 19:35:46 +0200

    added email class

--- src/main/java/com/ventafri/Common/Email.java
+++ src/main/java/com/ventafri/Common/Email.java
@@ -1,4 +1,30 @@
 package com.ventafri.Common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Email {
+    private static List<String> emailAddresses = new ArrayList<>();
+
+    public String getEmails(){
+        StringBuilder emails = new StringBuilder();
+        if(emailAddresses.size() > 0){
+            emails.append("E-mail addresses: \n").append(String.join("\n", emailAddresses));
+            return emails.toString();
+        } else{
+            return "";
+        }
+    }
+
+    public String getEmail(int emailIndex){
+        return emailAddresses.get(emailIndex);
+    }
+
+    public void addEmail (String email){
+        emailAddresses.add(email);
+    }
+
+    public void modifyEmail(int emailIndex, String email){
+        emailAddresses.set(emailIndex, email);
+    }
 }
