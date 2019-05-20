21773e344bb90d4cae12d89006604accd0bae881
Susanna Ventafridda <ventafri@amazon.com>
2019-05-20 16:54:14 +0000

    started HashTable implementation

--- src/main/java/com/ventafri/hashtable/Contact.java
+++ src/main/java/com/ventafri/hashtable/Contact.java
@@ -1,4 +1,50 @@
 package com.ventafri.hashtable;
 
+import com.ventafri.Common.Address;
+import com.ventafri.Common.Email;
+import com.ventafri.Common.Phone;
+
 public class Contact {
+    private static String firstName = "";
+    private static String surname = "";
+    private static Phone phone;
+    private static Address address;
+    private static Email emailAddress;
+
+    private Contact (String inputName, String inputSurname){
+        this.firstName = inputName;
+        this.surname = inputSurname;
+        this.address = new Address();
+        this.emailAddress = new Email();
+    }
+
+    public String getAllContactDetails (){
+        StringBuilder contactInfo = new StringBuilder();
+        contactInfo.append(firstName).append(surname).append(Phone.getAllPhones()).append(Address.getAllAddresses
+        ).append(Email.getAllEmailAddresses).toString();
+        return contactInfo;
+    }
+
+    public String getName(){
+        return firstName;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public String getNameAndSurname(){
+        if (surname == ""){
+            return firstName;
+        }
+        return firstName + " " + surname;
+    }
+
+    public void setSurname(String inputSurname){
+        surname = inputSurname;
+    }
+
+    public void setName(String inputName){
+         firstName = inputName;
+    }
 }
--- src/main/java/com/ventafri/hashtable/PhoneBook.java
+++ src/main/java/com/ventafri/hashtable/PhoneBook.java
@@ -1,4 +1,27 @@
 package com.ventafri.hashtable;
+import java.util.Arrays;
 
 public class PhoneBook {
+    /* Initial capacity set to 16 as in Java HashMap */
+    private static int initialSize = 16;
+    private static int currentSize = 16;
+    /* Initialize bucket array containing keys*/
+    private static int[] keyArray = new int [16];
+    Arrays.fill(keyArray, null);
+    /* Initialize bucket array containing values*/
+    private static List<String> valueArray = new ArrayList<>();
+    /* load factor set to 2/3 of the storing capacity as in Java and Python */
+    private static int loadFactor = 0.75;
+    private static int numberOfContacts = 0;
+
+    private static float calculateLoadFactor(){
+        return numberOfContacts / currentSize;
+    }
+
+    private static void checkIfResizeNeeded(){
+        if (this.calculateLoadFactor() >= loadFactor){
+            keyArray
+
+        }
+    }
 }
